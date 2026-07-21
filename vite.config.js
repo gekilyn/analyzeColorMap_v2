b4f2902dcaafb67a4fe0b21f5395a1e63a984405
gekilyn <gekirinofficial@gmail.com>
2026-07-21 14:40:21 +0900

    DocsフォルダにBuild

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,15 @@ import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: ["./setupTests.js"],
     exclude: ["e2e/**", "**/*.e2e.test.*", "node_modules/**", "dist/**"],
+  },
+  build: {
+    outDir: "Docs",
   },
 });
